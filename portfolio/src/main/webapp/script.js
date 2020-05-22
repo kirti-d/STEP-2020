71009620adfe6c2e590baff652ba3e0d176ef680
kirti-d <kirtidabs5@gmail.com>
2020-05-22 23:25:31 +0530

    add js for scroll

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,6 +16,8 @@ var cube = document.getElementById("cube");
 var nextButton= document.getElementById("next");
 var currentClass = '';
 var idx=0;
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+var interval;
 
 // chane the side of cube on click
 function changeSide() {
@@ -31,3 +33,30 @@ function changeSide() {
 changeSide();
 
 nextButton.addEventListener( 'click', changeSide );
+
+for (var i = 0; i < navMenuAnchorTags.length; i++) {
+    navMenuAnchorTags[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowerCase();
+        console.log(this.textContent);
+        var targetSection = document.getElementById(targetSectionID);
+        console.log(targetSection);
+        //    interval = setInterval(scrollVertically, 20, targetSection);
+
+        interval = setInterval(function () {
+            scrollVertically(targetSection);
+        }, 20);
+    });
+}
+
+function scrollVertically(targetSection) {
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+    if (targetSectionCoordinates.top <= 0) {
+        clearInterval(interval);
+        return;
+    }
+    window.scrollBy(0, 50);
+}
+
+
+
